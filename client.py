7944d398fa55cbf7573398dbe7f729d56d5ed875
micha <me@michahere.com>
2020-07-10 22:51:27 +0200

    banathon 1.3

--- client.py
+++ client.py
@@ -59,25 +59,6 @@ async def on_message(message):
         if message.content.count(word) > 0 :
             print("%s has cursed" % (message.author))
             await message.channel.purge(limit=1)
-            await message.channel.send(f"{message.author} had been warned")
-            warned = discord.Embed(
-                title="You're warned",
-                description=f"You're warned on the {message.guild} server.",
-                colour=discord.Colour.from_rgb(250, 250, 0)
-            )
-
-            warned.set_footer(text=f"#Warned On {message.guild}")
-            warned.set_image(url="https://media.discordapp.net/attachments/619413581145833472/729690305648918568/ezgif-5-8f75a4ba685b.gif?width=622&height=350")
-            warned.set_thumbnail(
-                url="https://cdn.discordapp.com/avatars/727967252657471550/1fcacc779f361241eb5505e4de99ed81.png?size=128")
-            warned.set_author(name="Banathon",
-                            icon_url="https://cdn.discordapp.com/avatars/727967252657471550/1fcacc779f361241eb5505e4de99ed81.png?size=128")
-            warned.add_field(name="Reason", value=f"using bad words", inline=False)
-            warned.add_field(name="Info", value="If you want more information, please contact our staff.", inline=False)
-
-            await message.author.send(embed=warned)
-            print(f"Warned {message.author} for using bad words")
-            await bot.process_commands(message)
 
     if say_make_title == True:
         content = message.content
