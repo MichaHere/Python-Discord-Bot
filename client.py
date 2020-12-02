2fae02ed1a06a28323ad53262691ce5229ec021c
micha <me@michahere.com>
2020-12-02 15:57:17 +0100

    Banathon new

--- client.py
+++ client.py
@@ -728,7 +728,7 @@ async def displayembed(ctx):
 
     await ctx.send(embed=embed)
 
-@tasks.loop(seconds=1)
+@tasks.loop(seconds=60)
 async def status_change():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(next(status)))
 
